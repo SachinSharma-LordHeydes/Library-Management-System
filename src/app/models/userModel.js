aacff34fea6d4830bd8a552d568ca65d19b037d1
SachinSharma-LordHeydes <thereisamailforme@gmail.com>
2025-04-05 17:21:49 +0545

    creating link between clerk and mongodb

--- src/app/models/userModel.js
+++ src/app/models/userModel.js
@@ -19,44 +19,12 @@ const userSchema = new mongoose.Schema(
       type: String,
       enum: ["student", "admin"],
       default:"student",
-      required: true,
       index: true,
     },
-    // contactNumber: {
-    //   type: String,
-    //   required: true,
-    //   minLength: 10,
-    //   maxLength: 10,
-    //   match: [/^\d{10}$/, "Contact number must be exactly 10 digits"],
-    // },
     imageURL: {
       type: String,
       required: true, ////https://ui-avatars.com/api/?name=Sachin
     },
-    // address: {
-    //   type: String,
-    //   required: true,
-    //   minLength: 3,
-    //   maxLength: 70,
-    // },
-    // dateOfBirth: {
-    //   type: Number, //convert to Date
-    //   required: true,
-    // },
-    // faculty: {
-    //   type: String,
-    //   minlength: 2,
-    //   maxlength: 50,
-    //   validate: {
-    //     validator: function (value) {
-    //       if (this.role === "student" && !value) {
-    //         return false;
-    //       }
-    //       return true;
-    //     },
-    //     message: "Faculty is required for students",
-    //   },
-    // },
     borrowedBooks: [
       {
         type: mongoose.Types.ObjectId,
